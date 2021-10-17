a645f2b86f447cfecd4ddb5f2162f758603014ec
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-10-17 12:23:26 +0200

    plugins: add nvim-bqf

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,48 +1,60 @@
-lvim.plugins ={
+lvim.plugins = {
 
 	-- LSP and linting
 	{ "nvim-treesitter/nvim-treesitter-textobjects", event = "BufRead" },
 
+	{
+		"kevinhwang91/nvim-bqf",
+		event = "BufRead",
+		config = function()
+			require("bqf").setup({
+				auto_enable = true,
+				preview = {
+					auto_preview = false,
+				},
+			})
+		end,
+	},
 	{
 		"folke/persistence.nvim",
 		event = "VimEnter",
 		config = [[ require("persistence").setup({dir = vim.fn.stdpath("cache") .. "/lvim_sessions/" }) ]],
 	},
 	-- Search
-		{
-			"jvgrootveld/telescope-zoxide",
-		},
-		{
-			"ggandor/lightspeed.nvim",
-			event = "BufWinEnter",
-			config = [[require('user.lightspeed')]],
-		},
+	{
+		"jvgrootveld/telescope-zoxide",
+	},
+	{
+		"ggandor/lightspeed.nvim",
+		event = "BufWinEnter",
+		config = [[require('user.lightspeed')]],
+	},
 
 	-- TMUX and session management
-		{
-			"aserowy/tmux.nvim",
-			event = "VimEnter",
-			config = [[require('user.tmux').setup()]],
-		},
+	{
+		"aserowy/tmux.nvim",
+		event = "VimEnter",
+		config = [[require('user.tmux').setup()]],
+	},
 
 	-- UI
-		{
-			"karb94/neoscroll.nvim",
-			event = "BufRead",
-			config = function()
-				require("neoscroll").setup({ respect_scrolloff = true })
-			end,
-		},
-		{
-			"lukas-reineke/indent-blankline.nvim",
-			event = "VimEnter",
-			config = [[require('user.indent')]],
-		},
+	{
+		"karb94/neoscroll.nvim",
+		event = "BufRead",
+		config = function()
+			require("neoscroll").setup({ respect_scrolloff = true })
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "VimEnter",
+		config = [[require('user.indent')]],
+	},
 
 	-- GIT
-		{
-			"ruifm/gitlinker.nvim",
-			event = "BufWinEnter",
+	{
+		"ruifm/gitlinker.nvim",
+		event = "BufWinEnter",
 		config = [[require('gitlinker').setup()]],
-		},
+	},
 }
