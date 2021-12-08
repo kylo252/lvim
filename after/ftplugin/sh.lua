dc9019750e629e73631cc6614dfa6a67a1154185
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-12-08 17:52:20 +0100

    shfmt: update settings

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -1,5 +1,5 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup { { exe = "shfmt", args = { "-i", "2", "-ci" } } }
+formatters.setup { { exe = "shfmt", args = { "-i", "2", "-ci", "-bn" } } }
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { exe = "shellcheck" } }
