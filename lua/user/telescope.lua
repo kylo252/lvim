331d30b5a06c4ad2206070f6b2d9ef3e4dca98fb
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-12-08 17:51:40 +0100

    telescope: add missing mappings

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,5 +1,28 @@
 local M = {}
 
+local _, actions = pcall(require, "telescope.actions")
+
+lvim.builtin.telescope.defaults.mappings = {
+  i = {
+    ["<C-c>"] = actions.close,
+    ["<C-j>"] = actions.cycle_history_next,
+    ["<C-k>"] = actions.cycle_history_prev,
+    ["<S-Up>"] = actions.cycle_previewers_prev,
+    ["<S-Down>"] = actions.cycle_previewers_next,
+    ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+    ["<CR>"] = actions.select_default + actions.center,
+    -- ["<C-i>"] = my_cool_custom_action,
+  },
+  n = {
+    ["<C-c>"] = actions.close,
+    ["<S-Up>"] = actions.cycle_previewers_prev,
+    ["<S-Down>"] = actions.cycle_previewers_next,
+    ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+    -- ["<C-Space>"] = custom_actions.fuzzy_filter_results,
+    -- ["<C-i>"] = my_cool_custom_action,
+  },
+}
+
 function M.setup_z()
   local _, builtin = pcall(require, "telescope.builtin")
   local _, themes = pcall(require, "telescope.themes")
