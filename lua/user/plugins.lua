b7cd2f6a021f88d3f5c2d718945a65912affb220
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-01-08 13:06:11 +0100

    plugins: test neovim-session-manager

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -22,12 +22,19 @@ return {
     end,
   },
   {
-    "folke/persistence.nvim",
-    event = "VimEnter",
+    "Shatur/neovim-session-manager",
     config = function()
-      require("persistence").setup {
-        dir = get_cache_dir() .. "/lvim_sessions/",
+      require("session_manager").setup {
+        sessions_dir = get_cache_dir() .. "/lvim_sessions", -- The directory where the session files will be saved.
+        path_replacer = "__", -- The character to which the path separator will be replaced for session files.
+        colon_replacer = "++", -- The character to which the colon symbol will be replaced for session files.
+        -- autoload_mode = require("session_manager.config").AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+        autoload_mode = false,
+        autosave_last_session = true, -- Automatically save last session on exit.
+        autosave_ignore_not_normal = true, -- Plugin will not save a session when no writable and listed buffers are opened.
+        autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
       }
+      require("telescope").load_extension "sessions"
     end,
   },
   -- Search
