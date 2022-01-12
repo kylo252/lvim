be7d0e941f41d99c75af3dad0f96f12a14e1ad70
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-01-12 10:22:26 +0100

    telescope: workaround for extensions lazy-loading

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -21,25 +21,13 @@ return {
       }
     end,
   },
-  {
-    "Shatur/neovim-session-manager",
-    config = function()
-      require("session_manager").setup {
-        sessions_dir = get_cache_dir() .. "/lvim_sessions", -- The directory where the session files will be saved.
-        path_replacer = "__", -- The character to which the path separator will be replaced for session files.
-        colon_replacer = "++", -- The character to which the colon symbol will be replaced for session files.
-        -- autoload_mode = require("session_manager.config").AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
-        autoload_mode = false,
-        autosave_last_session = true, -- Automatically save last session on exit.
-        autosave_ignore_not_normal = true, -- Plugin will not save a session when no writable and listed buffers are opened.
-        autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
-      }
-      require("telescope").load_extension "sessions"
-    end,
-  },
   -- Search
   {
     "jvgrootveld/telescope-zoxide",
+    after = "telescope.nvim",
+    config = function()
+      require("user.telescope").setup_z()
+    end,
   },
   {
     "ggandor/lightspeed.nvim",
@@ -55,6 +43,23 @@ return {
     event = "BufRead",
     config = function()
       require("user.tmux").setup()
+    end,
+  },
+  {
+    "Shatur/neovim-session-manager",
+    after = "telescope.nvim",
+    config = function()
+      require("session_manager").setup {
+        sessions_dir = get_cache_dir() .. "/lvim_sessions", -- The directory where the session files will be saved.
+        path_replacer = "__", -- The character to which the path separator will be replaced for session files.
+        colon_replacer = "++", -- The character to which the colon symbol will be replaced for session files.
+        -- autoload_mode = require("session_manager.config").AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+        autoload_mode = false,
+        autosave_last_session = true, -- Automatically save last session on exit.
+        autosave_ignore_not_normal = true, -- Plugin will not save a session when no writable and listed buffers are opened.
+        autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
+      }
+      -- require("telescope").load_extension "sessions"
     end,
   },
   -- UI
