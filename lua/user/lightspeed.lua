6e208ed06ba5e1f63fc98738e8f5da08ccc52b08
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-01-15 11:33:10 +0100

    lightspeed: adjust deprecated settings

--- lua/user/lightspeed.lua
+++ lua/user/lightspeed.lua
@@ -2,24 +2,20 @@ local M = {}
 
 function M.setup()
   require("lightspeed").setup {
-    exit_after_idle_msecs = { labeled = 1500, unlabeled = 1000 },
-    -- jump_to_first_match = true,
-
+    ignore_case = false,
+    exit_after_idle_msecs = { unlabeled = 1000, labeled = 1500 },
     -- s/x
-    grey_out_search_area = true,
-    highlight_unique_chars = true,
-    match_only_the_start_of_same_char_seqs = true,
-    jump_on_partial_input_safety_timeout = 400,
-    substitute_chars = { ["\r"] = "¬" },
-    -- Leaving the appropriate list empty effectively disables
-    -- "smart" mode, and forces auto-jump to be on or off.
-    cycle_group_fwd_key = "<tab>",
-    cycle_group_bwd_key = "<c-tab>",
+    jump_to_unique_chars = { safety_timeout = 400 }, -- jump right after the first input, if the target character is unique in the search direction
+    match_only_the_start_of_same_char_seqs = true, -- separator line will not snatch up all the available labels for `==` or `--`
+    substitute_chars = { ["\r"] = "¬" }, -- highlighted matches by the given characters
+    special_keys = { -- switch to the next/previous group of matches, when there are more matches than labels available
+      next_match_group = "<space>",
+      prev_match_group = "<tab>",
+    },
 
     -- f/t
-    limit_ft_matches = 4,
-    instant_repeat_fwd_key = nil,
-    instant_repeat_bwd_key = nil,
+    limit_ft_matches = 4, -- For 1-character search, the next 'n' matches will be highlighted after [count]
+    repeat_ft_with_target_char = false, -- repeat f/t motions by pressing the target character repeatedly
   }
 end
 
