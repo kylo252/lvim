db125a17dd31d2fbf316f1fc7e55fc6c33104f5c
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-03-03 11:28:28 +0100

    lualine: tweak lsp-progress

--- config.lua
+++ config.lua
@@ -26,7 +26,13 @@ local components = require "lvim.core.lualine.components"
 lvim.builtin.lualine.sections.lualine_y = { "location" }
 lvim.builtin.lualine.sections.lualine_a = { "filename" }
 lvim.builtin.lualine.sections.lualine_b = { components.branch }
-lvim.builtin.lualine.sections.lualine_c = { components.diff, "lsp_progress" }
+lvim.builtin.lualine.sections.lualine_c = {
+  components.diff,
+  {
+    "lsp_progress",
+    colors = { use = true },
+  },
+}
 ---}}}
 
 ---{{{ LSP
