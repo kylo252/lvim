1e1c8779efa81263da912f567f017abee788c361
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-03-04 15:46:02 +0100

    python: switch back to pyright

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,37 +1,16 @@
 local before_init = function(_, config)
   if vim.env.VIRTUAL_ENV then
-    local python_bin = require("lspconfig.util").path.join(vim.env.VIRTUAL_ENV, "bin", "python3")
+    local path_util = require("lspconfig.util").path
+    local python_bin = path_util.join(vim.env.VIRTUAL_ENV, "bin", "python3")
     config.settings.python.pythonPath = python_bin
   end
 end
 
-local pylsp_opts = {
-  -- autostart = false,
+local opts = {
   before_init = before_init,
   settings = {
-    pylsp = {
-      plugins = {
-        flake8 = {
-          enabled = true,
-        },
-        pyflakes = {
-          enabled = false,
-        },
-        pycodestyle = {
-          enabled = false,
-        },
-        pylint = {
-          enabled = false,
-        },
-        isort = {
-          enabled = true,
-        },
-        black = {
-          enabled = true,
-        },
-      },
-    },
+    pyright = {},
   },
 }
 
-require("lvim.lsp.manager").setup("pylsp", pylsp_opts)
+require("lvim.lsp.manager").setup("pyright", opts)
