720d93eb8075354f263925ab27f52241e60f882b
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-12-15 12:14:24 +0100

    cmp: test copilot

--- config.lua
+++ config.lua
@@ -76,4 +76,16 @@ table.insert(lvim.plugins, {
   end,
 })
 
+table.insert(lvim.plugins, {
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  requires = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup()
+      require("copilot_cmp").setup()
+    end, 100)
+  end,
+})
+
 ---}}}
