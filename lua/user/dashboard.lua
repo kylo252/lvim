1878330f1f76ceaa676c5d95eedc462202cb1624
kylo252 <59826753+kylo252@users.noreply.github.com>
2023-06-20 15:32:34 +0200

    chore: update icons

--- lua/user/dashboard.lua
+++ lua/user/dashboard.lua
@@ -3,16 +3,12 @@ lvim.builtin.alpha.mode = "dashboard"
 local scratch_file = "~/.config/lvim/lua/scratch.lua"
 
 lvim.builtin.alpha.dashboard.section.buttons.entries = {
-  { "SPC f f", "  Find File", "<CMD>Telescope find_files<CR>" },
-  { "SPC f r", "  Recently Used Files", "<CMD>Telescope oldfiles<CR>" },
+  { "SPC f f", "󰈞  Find File", "<CMD>Telescope find_files<CR>" },
+  { "SPC f r", "  Recently Used Files", "<CMD>Telescope oldfiles<CR>" },
   { "SPC f l", "  Find Word", "<CMD>Telescope live_grep<CR>" },
-  { "SPC f j", "  Recent Places", "<CMD>Telescope zoxide list<CR>" },
-  { "SPC P", "  Recent Projects", "<CMD>Telescope projects<CR>" },
-  { "SPC e", "  New File", "<CMD>ene!<CR>" },
-  { "SPC R", "  Load Session", "<CMD>SessionLoad<cr>" },
-  {
-    "SPC s",
-    "  Scratch",
-    "<CMD>edit " .. scratch_file .. "<CR>",
-  },
+  { "SPC f j", "  Recent Places", "<CMD>Telescope zoxide list<CR>" },
+  { "SPC P", "  Recent Projects", "<CMD>Telescope projects<CR>" },
+  { "SPC e", "  New File", "<CMD>ene!<CR>" },
+  { "SPC R", "󰀘  Load Session", "<CMD>SessionLoad<cr>" },
+  { "SPC s", "  Scratch", "<CMD>edit " .. scratch_file .. "<CR>" },
 }
